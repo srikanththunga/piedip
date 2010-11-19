b34f1d3d1a20c7b508ae0aba79cd1a365ce76d1a
srikanth thunga <srikanththunga@yahoo.com>
2010-11-19 12:39:59 +0530

    blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piedip::Application.config.secret_token = '88775439c0c0173473cba397efc466edab594a80c1dfb52e58c64f047451751738bf873b027628849d8687bd035750d5dad96b9c8cba72cdd32eae5be61b04b8'
